93f29ff0771bc6e80b6bb13e1335eb037d9a025c
Florent Beillonnet <florent.beillonnet@gmail.com>
2022-05-14 12:47:41 +0200

    working recognition with typescript

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -4,8 +4,8 @@
 
         <canvas id="can" class="main-page__canva" width="300" height="300"></canvas>
         <div class="main-page__buttons">
-            <!-- <button class="maint-page__erase" @click="erase()">Erase</button>
-            <button class="maint-page__undo" @click="undo()">Undo</button>-->
+            <button class="maint-page__erase" @click="erase()">Erase</button>
+            <button class="maint-page__undo" @click="undo()">Undo</button>
             <button class="maint-page__check" @click="check()">Check</button>
         </div>
         <div id="candidateList"></div>
@@ -14,17 +14,23 @@
     
 <script setup lang="ts">
 
-// import * as test from '../../kanjicanvas/docs/resources/javascript/kanji-canvas.min.js'
-// import '../../kanjicanvas/docs/resources/javascript/ref-patterns.js'
-
-import { inject, onMounted } from 'vue'
+import { onMounted } from 'vue'
 import { modKanjiCanvas } from './KanjiCanvaWraper'
 
-// const KanjiCanvas = inject('KanjiCanvas')
 let kanjiToMatch = "二"
+type iKanjiCanvas = {
+
+    recognize: (arg: string) => string;
+    init: (arg: string) => void;
+    deleteLast: (arg: string) => void;
+    erase: (arg: string) => void;
+
+}
+
+const KanjiCanvas = modKanjiCanvas as unknown as iKanjiCanvas;
 
 function check() {
-    let result = modKanjiCanvas.recognize('can');
+    let result = KanjiCanvas.recognize('can');
     if (result.includes(kanjiToMatch)) {
         console.log("SUCCESS");
     }
@@ -32,44 +38,18 @@ function check() {
     console.log(result);
 }
 
+function erase() {
+    KanjiCanvas.erase('can');
+
+}
+
+function undo() {
+    KanjiCanvas.deleteLast('can');
+}
+
 onMounted(() => {
-
-    modKanjiCanvas.init('can')
-    console.log("on est dans onmounted")
+    KanjiCanvas.init('can')
 })
-// export {
-//     inject: ['KanjiCanvas'],
-//         data() {
-//     return {
-//         kanjiToMatch: "二",
-
-//     }
-// },
-// methods: {
-//     erase() {
-//         this.KanjiCanvas.erase('can');
-//     },
-//     undo() {
-//         this.KanjiCanvas.deleteLast('can');
-//     },
-//     check() {
-//         let result = this.KanjiCanvas.recognize('can');
-//         if (result.includes(this.kanjiToMatch)) {
-//             console.log("SUCCESS");
-//         }
-
-//         console.log(result);
-//     }
-
-// },
-// mounted() {
-//     console.log("on est ici")
-//     // console.log(this.$kanjiCanvas)
-//     // this.kanjiCanvas = this.$kanjiCanvas;
-//     this.KanjiCanvas.init('can');
-
-// }
-// }
 </script>
     
 <style src="./MainPage.css" scoped >
