30549832b338a1446cec167fe91b7a57c21b1267
Florent Beillonnet <florent.beillonnet@gmail.com>
2022-06-15 18:02:31 +0200

    adding canva on verso + animation when drawing

--- src/components/Card/Verso.vue
+++ src/components/Card/Verso.vue
@@ -1,8 +1,9 @@
 !<template>
     <div class="verso">
         <h1 class="verso__title">{{ prop.translation }}</h1>
-        <div class="verso__kanji-div">
-            <div class="verso__kanji-text">{{ prop.kanji }}</div>
+        <div class="verso__kanji-div" :style="{ width: canvaWidth + 'px' }">
+            <!-- <div class="verso__kanji-text">{{ prop.kanji }}</div> -->
+            <canvas class="verso__kanji-text" id="verso_canva"></canvas>
         </div>
         <div class="verso__kanas">
             <div class="verso__onyomi-kunyomi">
@@ -20,7 +21,17 @@
 </template>
     
 <script setup lang="ts">
+import { ref, onMounted, onActivated, onUpdated } from 'vue';
+import { modKanjiCanvas } from '../KanjiCanvaWraper'
 import { flipCard, kanjiCard } from '../Deck/deck';
+
+var canvas: HTMLCanvasElement;
+var ctx: CanvasRenderingContext2D | null;
+
+var pencilSize = 40;
+
+const canvaWidth = ref(500)
+const canvaHeight = ref(500)
 
 interface Card extends kanjiCard {
     required?: true,
@@ -29,12 +40,73 @@ interface Card extends kanjiCard {
     //https://vuejs.org/guide/typescript/composition-api.html#typing-component-props
 }
 
+type iKanjiCanvas = {
+
+    recognize: (arg: string) => string;
+    init: (arg: string) => void;
+    deleteLast: (arg: string) => void;
+    erase: (arg: string) => void;
+}
+
+// Cast type for typescript compliance
+const KanjiCanvas = modKanjiCanvas as unknown as iKanjiCanvas;
 
 // const card = defineProps<Card>()
 
-defineProps<{ prop: Card }>()
+const props = defineProps<{ prop: Card }>()
+
+function drawResult(kanjiToGuess: string) {
+
+    console.log(kanjiToGuess)
+    if (canvas === undefined) {
+        throw console.error("canvas undefined")
+    }
+    if (ctx === null) {
+        throw console.error("canvas context null");
+    }
+    ctx.font = canvas.height * 0.7 + 'px bold serif';
+    ctx.textBaseline = 'hanging'
+    ctx.textAlign = 'center'
+    // ctx.strokeStyle = "Gainsboro"
+    ctx.fillStyle = "Gainsboro"
+    ctx.fillText(kanjiToGuess, canvas.width / 2, canvas.height / 3);
+    // Set again because recognize function changed it to draw 
+    // the numbers
+    ctx.lineWidth = pencilSize;
+
+}
+onUpdated(() => {
+    KanjiCanvas.erase('verso_canva');
+    drawResult(props.prop.kanji)
+})
 
 
+onMounted(() => {
+    try {
+        canvas = document.getElementById('verso_canva') as HTMLCanvasElement;
+        if (canvas === null) {
+            throw new Error("Canva not found")
+        }
+        ctx = canvas.getContext('2d');
+        if (ctx === null) {
+            throw new Error("Context of canva not found")
+        }
+    }
+    catch (Error) {
+        console.log(Error)
+        return;
+    }
+    // cssVarBorderWidth = 10;
+    console.log(canvaWidth.value);
+    console.log(canvaHeight.value);
+    ctx.canvas.width = canvaWidth.value; // substract the double of the border width
+    ctx.canvas.height = canvaHeight.value;
+
+    KanjiCanvas.init('verso_canva')
+
+    ctx.lineWidth = pencilSize;
+
+})
 
 </script>
     
